44f09b3a39586a325e75774adba3f6436fcaf99b
bznein <nikolas.degiorgis@interchain.io>
2024-06-27 15:26:41 +0100

    Added durl function

--- functions.sh
+++ functions.sh
@@ -36,6 +36,24 @@ dp() {
     docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}} {{$p}}  {{end}}' "${container}"
 }
 
+durl() {
+    container=$(get_container_name "$1")
+
+    res=$(docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}}{{$p}};{{end}}' "${container}")
+    IFS=";" read -rA ports <<< "$res"
+
+    declare -a array
+    for a in $ports; do
+        array+=(`echo $a | cut -d'/' -f1`)
+    done
+    port=$(printf "%s\n" "${array[@]}" | fzf)
+    url="http://localhost:"
+    url+=$port
+    echo $url
+    xdg-open $url >/dev/null
+
+}
+
 get_container_name(){
     if [ -n "$1" ]; then
         echo "$1"
